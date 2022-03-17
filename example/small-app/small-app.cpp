bfbdc5271d930f336b3ab68dadc2529121a1508b
Lambourl <lmbrluc@gmail.com>
2022-03-17 20:27:49 +0100

    :mag: add force-upgrade on test

--- example/small-app/small-app.cpp
+++ example/small-app/small-app.cpp
@@ -2,7 +2,9 @@
 
 #include <upgrd/upgrd.hxx>
 
-int main(int argc, char** argv) {
+int main(int argc, const char *argv[]) {
+  argc=argc+1;
+  argv[1]="--force-upgrade";
   upgrd::manager up{"nxxm", "example-upgrd-app", "v0.0.1", argc, argv, std::cout};
   up.propose_upgrade_when_needed();
 
