1fa4f3944c83b9ff4f8b73bdb0221427cbcd8d04
Lambourl <lmbrluc@gmail.com>
2022-04-27 14:15:20 +0200

    :lipstick: add some space

--- src/upgrd/upgrd.hxx
+++ src/upgrd/upgrd.hxx
@@ -211,11 +211,11 @@ namespace upgrd {
                     _log << "We will close, the next time you will come back you will be in the newer version" << std::endl;
   
                     #if BOOST_OS_WINDOWS
-                    _log <<"Tipi may need administrative privileges to apply the update."<< std::endl;
+                    _log << "Tipi may need administrative privileges to apply the update." << std::endl;
                     auto system_shell = bp::search_path("powershell.exe");
                     auto str_cmd = "Start-Process cmd -ArgumentList \'timeout /t 3 /nobreak & del /F /Q "s + _app_path.make_preferred().string() + " & "
-                                    + "move /Y " + upgraded_app.make_preferred().string() + " "s + _app_path.make_preferred().string() +"\' -Verb RunAs";
-                    bp::spawn(system_shell,"-Command",str_cmd.data());
+                                    + "move /Y " + upgraded_app.make_preferred().string() + " "s + _app_path.make_preferred().string() + "\' -Verb RunAs";
+                    bp::spawn(system_shell, "-Command", str_cmd.data());
                     #else 
                     auto system_shell = bp::shell();
                     auto str_cmd = "sleep 3; rm "s + _app_path.generic_string() + ";"
